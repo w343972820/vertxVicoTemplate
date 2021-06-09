d863faa3c3b8ef6912e241b8bbdcf57f037391be
w343972820 <343972820@qq.com>
2021-06-09 16:52:29 +0800

    分开参数

--- src/main/java/vicoMain/Sql_Config_Log_Web_Time.java
+++ src/main/java/vicoMain/Sql_Config_Log_Web_Time.java
@@ -10,18 +10,23 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.ResultSet;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.redis.RedisClient;
+import io.vertx.redis.RedisOptions;
 import utils.JdbcUtils;
 import utils.Utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
+
 
 public class Sql_Config_Log_Web_Time  extends AbstractVerticle {
     InternalLogger logger = Log4JLoggerFactory.getInstance(Sql_Config_Log_Web_Time.class);
     JDBCClient jdbcClient;
     JsonObject jsonConfig;
+
+    RedisClient redisClient;
     @Override
     public void start() throws Exception {
         // 创建HttpServer
@@ -33,46 +38,31 @@ public class Sql_Config_Log_Web_Time  extends AbstractVerticle {
         router.route().consumes("application/json");
         router.route().produces("application/json");
         jdbcClient = new JdbcUtils(vertx).getDbClient();
-        //读取配置
-
-        //新建post请求
-        router.post("/vicoPostTest").handler(req->{
-            //对客户端传入的参数做json处理
-            JsonObject resultsJson = JdbcUtils.param(req);
-            //判断服务器传过来的json
-            String putuuid = resultsJson.getString("putuuid");
-            //可以先对putuuid判断，在响应相应的值
-            sqlQury(putuuid).setHandler(h->{
-                if (h.succeeded()){
-                    if (h.result()){
-                        req.response()
-                                .putHeader("content-type", "application/json")
-                                .end(new JsonObject().put("code","2000").put("msg","找到相同:"+putuuid).toString());
-                    }else{
-                        req.response()
-                                .putHeader("content-type", "application/json")
-                                .end(new JsonObject().put("code","2000").put("msg","末在数据库内找到相同UUID").toString());
-                    }
-                }else{
-                    System.out.println("数据库查询出错");
-                    req.response()
-                            .putHeader("content-type", "application/json")
-                            .end(new JsonObject().put("code","1000").put("msg","数据库异常"+h.cause()).toString());
-                }
-            });
-        });
+        RedisOptions config = new RedisOptions().setHost("192.168.1.69").setPort(6379).setAuth("oQFMvr*QgoX4vznj");
+        redisClient = RedisClient.create(vertx, config);
+        VicoDao vicoDao = new VicoDao(jdbcClient);
+        //新建post请求,在chuLiWeb里面处理,前面是类对象，后面是方法
+        router.post("/vicoPostTest").handler(vicoDao::chuLiWeb);
         // 把请求交给路由处理-----
         server.requestHandler(router::accept);
         // 设置端口
         server.listen(8877);
         //设置定时期，定期执行
         this.setStatus1();
+        redisTest();
     }
+
     //设置每分钟执行一次
     private void setStatus1(){
         this.vertx.setPeriodic(60000,handler->{
             lunXunShuJuKu();
         });
+    }
+    private void redisTest(){
+        this.vertx.setPeriodic(60000,handler->{
+
+        });
+
     }
     private void lunXunShuJuKu() {
         //读取配置，打包成jar后放入服务器，可在服务器同级目录新建conf目录，及conifg.json文件，优先读取文件
@@ -81,41 +71,7 @@ public class Sql_Config_Log_Web_Time  extends AbstractVerticle {
     }
 
 
-    //用future返回回调，以保证请求及处理同步问题
-    public Future<Boolean> sqlQury(String webUuid){
-        Future<Boolean> future = Future.future();
-        if (webUuid==null || webUuid==""){
-            future.complete(false);
-            return future;
-        }
-        String sqlUid = "select putuuid from tradeWithDraw";
-        //String sql = "insert into tradeWithDraw(putuuid,cointypename,putnum,apitype,putaddress,status,createTime) values (?,?,?,?,?,0,?)";
-        //String updatesqls = "update tradeWithDraw set status = "+status+",msg=? where putuuid = \'" +txID +"\'";
-        //JsonArray params = new JsonArray().add(msg);
-        jdbcClient.query(sqlUid,qurSql->{
-            if (qurSql.succeeded()){
-                // 获取到查询的结果，Vert.x对ResultSet进行了封装
-                ResultSet resultSet = qurSql.result();
-                // 把ResultSet转为List<JsonObject>形式
-                List<JsonObject> resultsList = resultSet.getRows();
-                for(JsonObject json:resultsList){
-                    System.out.println(json.toString());
-                    if (webUuid.equals(json.getString("putuuid"))){
-                        future.complete(true);
-                        break;
-                    }
-                }
-                if (future.result()==null){
-                    future.complete(false);
-                }
-            }else{
-                logger.error(qurSql.cause());
-                future.complete(false);
-                future.failed();
-            }
-        });
-        return future;
-    }
+
     public Future<Boolean> readConfig(Vertx vertx) {
         Future<Boolean> future = Future.future();
         Utils.getConfig(vertx, "conf/config.json").setHandler(handler->{
--- src/main/java/vicoMain/VicoDao.java
+++ src/main/java/vicoMain/VicoDao.java
@@ -1,4 +1,80 @@
 package vicoMain;
 
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.Log4JLoggerFactory;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.web.RoutingContext;
+import utils.JdbcUtils;
+
+import java.util.List;
+
 public class VicoDao {
+    InternalLogger logger = Log4JLoggerFactory.getInstance(Sql_Config_Log_Web_Time.class);
+    JDBCClient jdbcClient;
+    public VicoDao(JDBCClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+    public void chuLiWeb(RoutingContext req){
+        //对客户端传入的参数做json处理
+        JsonObject resultsJson = JdbcUtils.param(req);
+        //判断服务器传过来的json
+        String putuuid = resultsJson.getString("putuuid");
+        //可以先对putuuid判断，在响应相应的值
+        sqlQury(putuuid).setHandler(h->{
+            if (h.succeeded()){
+                if (h.result()){
+                    req.response()
+                            .putHeader("content-type", "application/json")
+                            .end(new JsonObject().put("code","2000").put("msg","找到相同:"+putuuid).toString());
+                }else{
+                    req.response()
+                            .putHeader("content-type", "application/json")
+                            .end(new JsonObject().put("code","2000").put("msg","末在数据库内找到相同UUID").toString());
+                }
+            }else{
+                System.out.println("数据库查询出错");
+                req.response()
+                        .putHeader("content-type", "application/json")
+                        .end(new JsonObject().put("code","1000").put("msg","数据库异常"+h.cause()).toString());
+            }
+        });
+    }
+    //用future返回回调，以保证请求及处理同步问题
+    public Future<Boolean> sqlQury(String webUuid){
+        Future<Boolean> future = Future.future();
+        if (webUuid==null || webUuid==""){
+            future.complete(false);
+            return future;
+        }
+        String sqlUid = "select putuuid from tradeWithDraw";
+        //String sql = "insert into tradeWithDraw(putuuid,cointypename,putnum,apitype,putaddress,status,createTime) values (?,?,?,?,?,0,?)";
+        //String updatesqls = "update tradeWithDraw set status = "+status+",msg=? where putuuid = \'" +txID +"\'";
+        //JsonArray params = new JsonArray().add(msg);
+        jdbcClient.query(sqlUid,qurSql->{
+            if (qurSql.succeeded()){
+                // 获取到查询的结果，Vert.x对ResultSet进行了封装
+                ResultSet resultSet = qurSql.result();
+                // 把ResultSet转为List<JsonObject>形式
+                List<JsonObject> resultsList = resultSet.getRows();
+                for(JsonObject json:resultsList){
+                    System.out.println(json.toString());
+                    if (webUuid.equals(json.getString("putuuid"))){
+                        future.complete(true);
+                        break;
+                    }
+                }
+                if (future.result()==null){
+                    future.complete(false);
+                }
+            }else{
+                logger.error(qurSql.cause());
+                future.complete(false);
+                future.failed();
+            }
+        });
+        return future;
+    }
 }
